c4a343fc527dda9f1621af53886774719b3c9b06
HyeWon <un0211@kaist.ac.kr>
2017-07-11 02:31:08 +0900

    PutTask, DeleteTask. 첫 로그인시 문제점 있음

---
+++ app/src/main/java/com/example/q/cs496_pj2_1/DeleteTask.java
@@ -0,0 +1,8 @@
+package com.example.q.cs496_pj2_1;
+
+/**
+ * Created by q on 2017-07-11.
+ */
+
+public class DeleteTask {
+}
---
+++ app/src/main/java/com/example/q/cs496_pj2_1/PutTask.java
@@ -0,0 +1,8 @@
+package com.example.q.cs496_pj2_1;
+
+/**
+ * Created by q on 2017-07-10.
+ */
+
+public class PutTask {
+}
